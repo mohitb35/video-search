a21d16e847a677e1d36e232fe3b59c7a54502e28
Mohit Bajaj <mohit_b35@hotmail.com>
2020-04-16 11:24:14 +0530

    add searchbar functionality - include axios - pass onSubmit prop from App to SearchBar - define onSearchSubmit method to call Youtube search API in App - call onSearchSubmit via onSubmit prop sent to SearchBar - youtube API axios config

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,11 +2,32 @@ import React from 'react';
 
 import SearchBar from './SearchBar';
 
+import youtube from '../apis/youtube';
+import config from '../config';
+
 class App extends React.Component {
+
+	onSearchSubmit(term) {
+		console.log(term);
+		youtube.get('/search', {
+			params: {
+				key: config.KEY,
+				part: 'snippet',
+				q: term,
+				maxResults: 10,
+				type: 'video'
+			}
+		})
+		.then(results => {
+			console.log(results.data.items);
+		})
+		;
+	}
+
 	render() {
 		return (
 			<div className="ui container">
-				<SearchBar /> 
+				<SearchBar onSubmit={this.onSearchSubmit}/> 
 			</div>
 		)
 	}
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -11,10 +11,17 @@ class SearchBar extends React.Component {
 		});
 	}
 
+	onFormSubmit = (event) => {
+		event.preventDefault();
+		console.log(this.state.term);
+		// call form submit callback method passed from App
+		this.props.onSubmit(this.state.term);
+	}
+
 	render() {
 		return (
 			<div className="ui segment">
-				<form className="ui form">
+				<form className="ui form" onSubmit={this.onFormSubmit}>
 					<div className="field">
 						<label>Video Search</label>
 						<input 
